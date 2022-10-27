c3b590f51a34860285005db8b65dbbf795df2276
Lucas <testonilucas015@gmail.com>
2022-10-27 13:58:34 -0300

    Atualização de Bugs Pagina Home

--- target/classes/static/frontend/home/main.js
+++ target/classes/static/frontend/home/main.js
@@ -1,10 +1,14 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-}
+//Animação Browse-category left show
+$(".browse-category").click(function(){
+    $("#mySidebar").animate({width: '250px'});
+});
+//Animação Browse-category left hide
+$(".closebtn").click(function(){
+    $("#mySidebar").animate({width: '0px'});
+});
 
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-}
+//Animação Menu Dropdown
 $('.profile-menu').on('click', function() {
     $('#SidepanelMenu').slideToggle('slow');
-});
+})
+
