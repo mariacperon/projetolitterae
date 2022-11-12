92d1a74df9713986494d0bf8ee02d81f57f9181b
Lucas <testonilucas015@gmail.com>
2022-11-12 17:57:29 -0300

    Lucas: Inserção Tela de Pesquisa por Genero + Configuração Home

--- src/main/resources/static/frontend/home/js/main-home.js
+++ src/main/resources/static/frontend/home/js/main-home.js
@@ -1,13 +1,46 @@
 //Animação Browse-category left show
 $(".browse-category").click(function(){
-    $("#mySidebar").animate({width: '250px'});
+    $(".sidebar").animate({width: '250px'});
 });
 //Animação Browse-category left hide
 $(".closebtn").click(function(){
-    $("#mySidebar").animate({width: '0px'});
+    $(".sidebar").animate({width: '0px'});
 });
 
 //Animação Menu Dropdown
 $('.profile-menu').on('click', function() {
     $('#SidepanelMenu').slideToggle('slow');
+})
+//Menu Active Leituras da Semana
+document.querySelectorAll(".book-type").forEach( function(button) {
+
+    button.addEventListener("click", function(event) {
+        const el = event.target || event.srcElement;
+        const id = el.id;
+        console.log(id);
+        $('.book-type').removeClass('active');
+        $(this).addClass('active');
+    });
+})
+//Função Scroll de Livros
+$('.popular-books').scroll(function (event) {
+    event.preventDefault()
+    var scroll = $(this).scrollTop();
+    if (scroll > 0){
+        $('.book-slide').fadeOut('500');
+        document.querySelector(".main-wrapper").style.marginTop='0';
+    }else if (scroll == 0){
+        $('.book-slide').fadeIn('500');
+        document.querySelector(".main-wrapper").style.marginTop='100px';
+    }
+});
+
+document.querySelectorAll(".sidebar").forEach( function(button) {
+    button.addEventListener("click", function(event) {
+        const select = event.target || event.srcElement;
+        const id = select.id;
+        if(id >=0 && id <=17)
+            sessionStorage.setItem('tipoGenero', id);
+             window.location.href = 'pesquisaGen.html';
+    });
 })
---
+++ src/main/resources/static/frontend/home/js/pesquisaGen.js
@@ -0,0 +1,87 @@
+//var idUsuario = sessionStorage.getItem("idUsuario")
+var IdGenero = sessionStorage.getItem("tipoGenero")
+var idUsuario = "100001"
+var campo = "genero"
+
+
+//Animação Browse-category left show
+$(".browse-category").click(function () {
+    $(".sidebar").animate({ width: '250px' });
+});
+//Animação Browse-category left hide
+$(".closebtn").click(function () {
+    $(".sidebar").animate({ width: '0px' });
+});
+
+//Animação Menu Dropdown
+$('.profile-menu').on('click', function () {
+    $('#SidepanelMenu').slideToggle('slow');
+})
+//-------------------------------------------------
+/*Eventos Pagina Responsiva*/
+
+document.querySelectorAll(".sidebar").forEach( function(button) {
+    button.addEventListener("click", function(event) {
+        const select = event.target || event.srcElement;
+        const id = select.id;
+        if(id >=0 && id <=17)
+        sessionStorage.setItem('tipoGenero', id);
+        location.reload()
+    });
+})
+//window.location.href = '';
+fetch(`http://localhost:80/livro/pesquisarpor?idUsuario=${idUsuario}&campo=genero&value=${IdGenero}`, {method: 'GET',})
+    .then(response => response.text())
+    .then(function resultado(result) {
+        var bd_result = JSON.parse(result)
+        createElementBook(bd_result)
+    })
+    .catch(error => console.log('error', error));
+
+
+function createElementBook(bd_result) {
+    var leiamais_limit = 60;
+    //Variavel Comando Div
+    let product = '';
+    //Inciar Contado Para Definir Cor
+    let cont = 0
+    //Incia Color
+    let color = ""
+    
+    
+    for (let i = 0; i < bd_result.length; i++) {
+        
+        if (bd_result[i].sinopse.length > 90){
+            bd_result[i].sinopse = bd_result[i].sinopse.substring(0,90)+"...";
+        }
+        cont++
+        if (cont == 1) {
+            color = "#cbb5e2"
+        } if (cont == 2) {
+            color = "#fbadaf"
+        } else if (cont == 3) {
+            color = "#fdca95"
+        } else if (cont == 4) {
+            color = "#fbadaf"
+        } else if (cont == 5) {
+            color = "#fdca95"
+        } else if (cont == 6) {
+            color = "#cbb5e2"
+            cont = 0
+        }
+        product += `
+    <div style="background-color: ${color};" class="book-product">
+    <div class="book-image">
+        <img class="book-photo" src="${bd_result[i].imagem}">
+    </div >
+    <div class="book-detail">
+    <div class="Titulo-book">${bd_result[i].nome}</div>
+    <div class="autor-book">Autor: ${bd_result[i].autor} </div>
+    <div class="sinopse-book">${bd_result[i].sinopse}</div>
+    <div style="color: ${color};" id= "${bd_result[i].id}"class="book-reserva">Reservar</div>
+    </div>
+   </div>
+  `;
+    }
+    $(".book-list").append(product);
+}
